26271328ae038d338b29fc70505220d474696fa7
amandadupell <anddupell@gmail.com>
2021-05-21 16:58:39 -0400

    more things

--- pages/dev.js
+++ pages/dev.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { AppContainer, InternIntro } from "../components";
 
-import { Box, Heading, Paragraph, Image } from "grommet";
+import { Box, Heading, Paragraph, Image, Text } from "grommet";
 
 export default function Home() {
   return (
@@ -21,27 +21,31 @@ export default function Home() {
         <Box
           background="background-contrast"
           align="center"
-          margin={{ vertical: "xlarge" }}
           pad={{ vertical: "medium" }}
         >
-        <Box align="center" justify="center">
-          <Image
-            src="/Quotation Marks.svg"
-            width="56"
-            height="40"
-            a11yTitle="Quotation Marks"
-            opacity="medium"
-          />
+          <>
+            <Box align="center" justify="center">
+              <Image
+                src="/Quotation Marks.svg"
+                width="56"
+                height="40"
+                a11yTitle="Quotation Marks"
+                opacity="medium"
+              />
+            </Box>
+            <Paragraph size="large" textAlign="center">
+              Hello! My name is Amanda Dupell and I am super excited to start
+              working as a UI/UX Developer Intern as a part of HPE's Experience 
+              Studio and Design System team!
+            </Paragraph>
+          </>
         </Box>
-        <Box>
-          <Paragraph weight="bold" size="large" textAlign="center">
-            <b>
-              My name is Amanda Dupell and I am super excited to start working as a UI/UX
-              Developer Intern as a part of HPE's Experience Studio and Design System team!
-            </b>
-          </Paragraph>
+        <Box margin={{ top: "xlarge" }}>
+          <Text weight="bold" size="xlarge">Things I'll be working on!</Text>
         </Box>
-      </Box>
+        <Paragraph size="medium">
+          Here are some examples of the 
+        </Paragraph>
       </Box>
     </AppContainer>
   );
