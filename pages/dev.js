5740ec5d3a4da43e7012870bf8d95fe0a3403759
amandadupell <anddupell@gmail.com>
2021-05-24 19:26:52 -0400

    added to bio page and image in public folder

--- pages/dev.js
+++ pages/dev.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import { AppContainer, InternIntro } from "../components";
 
-import { Box, Heading, Paragraph, Image, Text, Anchor, Card, CardBody } from "grommet";
+import { Box, Heading, Paragraph, List, Text, Anchor, Card, CardBody, CardFooter, Image } from "grommet";
 
 export default function Home() {
   return (
     <AppContainer>
       <Box pad={{ horizontal: "large" }}>
+        <Heading>Developer Intro</Heading>
         <InternIntro
           src="./amanda.jpeg"
           name="Amanda Dupell"
@@ -27,16 +28,86 @@ export default function Home() {
           learning opportunities to look forward to this summer and beyond. One of
           the first things I will be working on is the continuous improvement of 
           accessibility across {
-            <Anchor href="https://github.com/grommet/grommet">grommet</Anchor>
+            <Anchor href="https://github.com/grommet/grommet">Grommet</Anchor>
           } components. This process begins by researching and diving head first 
           into development tools to real-time inspect the web pages and 
           components I will be contributing to and building. 
         </Paragraph>
-        <Box pad={{ vertical: "large" }}>
-          <Card pad="small" gap="medium">
-            <CardBody>body</CardBody>
+        <Box pad={{vertical: "large"}}>
+          <Card width="large" elevation="xlarge">
+            <CardBody height="medium">
+              <Image
+                fit="cover"
+                src="/waveexample.png"
+                a11yTitle="WAVE accessibility tool screenshot example"
+              />
+            </CardBody>
+            <CardFooter background="#000000A0" width="large" justify="start">
+              <Text>
+                An example using the {
+                  <Anchor href="https://wave.webaim.org/">
+                    WAVE accessibility tool
+                  </Anchor>
+                } to evaluate a work-in-progress webpage. Using tools like this 
+                provides direct feedback and resources to fixing issues with 
+                your application's accessibility.
+              </Text>
+            </CardFooter>
           </Card>
         </Box>
+        <Paragraph size="medium">
+          Here is a list of helpful links in relation to accessibility and 
+          how HPE is directly addressing and making conftirubtions to better our 
+          code:
+        </Paragraph>
+        <Box align="start" pad={{left: "medium"}}>
+          <List
+              primaryKey="item"
+              data={[
+                { item:
+                  <Paragraph size="small" >
+                    - {
+                      <Anchor href="https://github.com/grommet/grommet/issues/4068">
+                        Grommet Accessibility Audit:
+                      </Anchor>
+                    } An open issue in our Grommet repo to help identify accessibility
+                    improvements that can be made to better align the library with{" "}
+                    {
+                      <Anchor href="https://www.w3.org/TR/WCAG21/">
+                        WCAG standards
+                      </Anchor>
+                    }
+                  </Paragraph> 
+                },
+                { item:
+                  <Paragraph size="small" >
+                    - {
+                      <Anchor href="https://design-system.hpe.design/foundation/accessibility">
+                        HPE Design System Accessibility Principles and Resources:
+                      </Anchor>
+                    } An overlook of HPE's commitment to continuously bettering our 
+                    accessibility as well as a list of helpful resources for developers to 
+                    use on their own to learn best practice and latest information
+                    in-industry
+                  </Paragraph>
+                }
+              ]}
+            />
+          </Box>
+          <Paragraph size="medium">
+            Learning more about accessibility standards and improving upon them
+            in Grommet will allow me to build a foundation of knowledge and comfort
+            within the framework. From here, my next focus will be to work with {
+              <Anchor href="https://design-system.hpe.design/">
+                HPE's Design System
+              </Anchor>
+            }, created entirely with the Grommet framework.
+          </Paragraph>
+          <Paragraph size="medium">
+            Later into my internship, I will be working alongside my fellow 2021
+            intern, Kenny, to dive deeper into this design system and make 
+            meaningful contributions to launch HPE forward. 
+          </Paragraph>
       </Box>
     </AppContainer>
   );
