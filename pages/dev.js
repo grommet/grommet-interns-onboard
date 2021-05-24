a17f68f5816c6dfc7127a5535620962aface93b7
amandadupell <anddupell@gmail.com>
2021-05-24 07:32:41 -0400

    more stuff

--- pages/dev.js
+++ pages/dev.js
@@ -2,13 +2,12 @@ import React from "react";
 
 import { AppContainer, InternIntro } from "../components";
 
-import { Box, Heading, Paragraph, Image, Text } from "grommet";
+import { Box, Heading, Paragraph, Image, Text, Anchor, Card, CardBody } from "grommet";
 
 export default function Home() {
   return (
     <AppContainer>
       <Box pad={{ horizontal: "large" }}>
-        <Heading>Developer Intro</Heading>
         <InternIntro
           src="./amanda.jpeg"
           name="Amanda Dupell"
@@ -18,34 +17,26 @@ export default function Home() {
           email="amanda.dupell@hpe.com"
           linkedIn="https://www.linkedin.com/in/amanda-dupell-profile/"
         />
-        <Box
-          background="background-contrast"
-          align="center"
-          pad={{ vertical: "medium" }}
-        >
-          <>
-            <Box align="center" justify="center">
-              <Image
-                src="/Quotation Marks.svg"
-                width="56"
-                height="40"
-                a11yTitle="Quotation Marks"
-                opacity="medium"
-              />
-            </Box>
-            <Paragraph size="large" textAlign="center">
-              Hello! My name is Amanda Dupell and I am super excited to start
-              working as a UI/UX Developer Intern as a part of HPE's Experience 
-              Studio and Design System team!
-            </Paragraph>
-          </>
-        </Box>
-        <Box margin={{ top: "xlarge" }}>
-          <Text weight="bold" size="xlarge">Things I'll be working on!</Text>
-        </Box>
-        <Paragraph size="medium">
-          Here are some examples of the 
+        <Text size="4xl" weight="bold" >Hi, there!</Text>
+        <Paragraph size="large">My name is Amanda Dupell and I am super excited to start
+          working as a UI/UX Developer Intern as a part of HPE's Experience 
+          Studio and Design System team!
         </Paragraph>
+        <Paragraph size="medium">
+          As I begin my internship at HPE, I have a lot of exciting projects and 
+          learning opportunities to look forward to this summer and beyond. One of
+          the first things I will be working on is the continuous improvement of 
+          accessibility across {
+            <Anchor href="https://github.com/grommet/grommet">grommet</Anchor>
+          } components. This process begins by researching and diving head first 
+          into development tools to real-time inspect the web pages and 
+          components I will be contributing to and building. 
+        </Paragraph>
+        <Box pad={{ vertical: "large" }}>
+          <Card pad="small" gap="medium">
+            <CardBody>body</CardBody>
+          </Card>
+        </Box>
       </Box>
     </AppContainer>
   );
