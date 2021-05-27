c1069552352fc0d2d57c6360477f04cec9ab26f0
amandadupell <anddupell@gmail.com>
2021-05-27 11:23:28 -0400

    added headers and content for design page

--- pages/design.js
+++ pages/design.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import { AppContainer, InternIntro } from "../components";
 
-import { Box, Heading } from "grommet";
+import { Box, Heading, Paragraph } from "grommet";
 
 export default function Home() {
   return (
     <AppContainer>
       <Box pad={{ horizontal: "large" }}>
-        <Heading>Designer Intro</Heading>
+        <Heading>Designer Intro & Project</Heading>
+        <Heading level="2">Meet Kenny Tran</Heading>
         <InternIntro
           src="./kenny.jpeg"
           name="Kenny Tran"
@@ -18,6 +19,13 @@ export default function Home() {
           //   email="TBD"
           linkedIn="https://www.linkedin.com/in/kennytrand/"
         />
+        <Heading level="2">The Design challenges for the 2021 Internship</Heading>
+        <Heading level="3">First Phase - HPE Design System Notifications</Heading>
+        <Paragraph size="large">
+          The 2021 interns of both Design & Development will join hands to dive
+          deeper into the world of product teams challenges by building the
+          Notification component for the HPE Design System.
+        </Paragraph>
       </Box>
     </AppContainer>
   );
