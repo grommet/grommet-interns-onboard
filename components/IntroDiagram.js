2993269eec0ea813819d4efb8253974b551444fe
amandadupell <anddupell@gmail.com>
2021-06-22 14:57:50 -0400

    added a11y to icons

--- components/IntroDiagram.js
+++ components/IntroDiagram.js
@@ -64,7 +64,7 @@ export const IntroDiagram = () => (
                         'going wide' and then zoning in. This allows for acquiring a large
                         sample size of what research exists in the world currently and
                         where HPE wants to take it."
-                    icon={<TestDesktop id="research" size="large" />}
+                    icon={<TestDesktop a11yTitle="research icon" id="research" size="large" />}
                 />
                 <IconBox 
                     title="Design"
@@ -72,14 +72,14 @@ export const IntroDiagram = () => (
                     content="Once there is a general idea of the best solution, design moves
                         forward with ideation. Those with UX/UI specialization factor in to 
                         ensure a seamless experience for the final designs."
-                    icon={<Edit id="design" size="large" />}
+                    icon={<Edit a11yTitle="pencil icon" id="design" size="large" />}
                 />
                 <IconBox 
                     title="Design System"
                     content="From design, the Design System works to ensure anyone 
                     at HPE understands how they can use these designs in the context 
                     of our company."
-                    icon={<System id="design-system" size="large" />}
+                    icon={<System a11yTitle="computer system icon" id="design-system" size="large" />}
                 />
                 <IconBox 
                     title="Development"
@@ -87,7 +87,7 @@ export const IntroDiagram = () => (
                         the final vision into a reality. At this point, development
                         has done research on their end to know the best implementation
                         for the design, adhering to the industry's best and latest practices."
-                    icon={<Code id="dev" size="large" />}
+                    icon={<Code a11yTitle="code script icon" id="dev" size="large" />}
                 />
             </Box>
         </Stack>
